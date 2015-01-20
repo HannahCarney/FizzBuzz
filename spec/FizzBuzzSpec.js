91fdaf62561eec726f6beaacca38801864c9d549
Hannah Carney <hannahcarneyart@gmail.com>
2015-01-20 13:42:31 +0000

    Third way of writing FizzBuzz

--- spec/FizzBuzzSpec.js
+++ spec/FizzBuzzSpec.js
@@ -2,6 +2,39 @@ describe("FizzBuzz", function() {
 
 	var fizzbuzz; 
 
+	describe("knows when a number", function() {
+
+		it("is divisible by 3", function() {
+			fizzbuzz = new FizzBuzz();
+			//fizzbuzz = FizzBuzz.new
+			expect(fizzbuzz.isDivisibleByThree(3)).toBe(true);
+		});
+
+		it("is NOT divisible by 3", function(){
+			fizzbuzz = new FizzBuzz();
+			expect(fizzbuzz.isDivisibleByThree(1)).toBe(false);
+		});
+	
+        it("is divisible by 5", function() {
+        	fizzbuzz = new FizzBuzz();
+        	expect(fizzbuzz.isDivisibleByFive(5)).toBe(true);	
+        });
+	    
+	    it("is NOT divisble by 5", function() {
+	    	fizzbuzz = new FizzBuzz();
+	    	expect(fizzbuzz.isDivisibleByFive(1)).toBe(false);
+	    });
+
+	    it("is divisible by 15", function() {
+	    	fizzbuzz = new FizzBuzz();
+	    	expect(fizzbuzz.isDivisibleByFifteen(15)).toBe(true);
+	    });
+
+	    it("is NOT divisible by 15", function() {
+	    	fizzbuzz = new FizzBuzz();
+	    	expect(fizzbuzz.isDivisibleByFifteen(1)).toBe(false);
+	    });
+	});
 
 	describe("knows when number, Fizz, Buzz, or FizzBuzz", function() {
 
